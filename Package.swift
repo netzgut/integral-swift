1b1c06da791288fa05cbd392ab98d59e2b803954
Ben Weidig <ben@netzgut.net>
2023-11-03 14:09:18 +0100

    BREAKING: platform iOS 12 -> 15

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(name: "IntegralSwift",
-                      platforms: [.iOS(.v12)],
+                      platforms: [.iOS(.v15)],
                       products: [
                           .library(name: "IntegralSwift",
                                    targets: ["IntegralSwift"])
