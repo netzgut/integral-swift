c6c3587263b889bd58efe42d1da12633f7acdbb2
Ben Weidig <ben@netzgut.net>
2024-01-02 15:10:37 +0100

    add PrivacyInfo.xcprivacy

--- Package.swift
+++ Package.swift
@@ -5,14 +5,16 @@ import PackageDescription
 let package = Package(name: "IntegralSwift",
                       platforms: [.iOS(.v15)],
                       products: [
-                          .library(name: "IntegralSwift",
-                                   targets: ["IntegralSwift"])
+                        .library(name: "IntegralSwift",
+                                 targets: ["IntegralSwift"])
                       ],
                       dependencies: [],
                       targets: [
-
-                          .target(name: "IntegralSwift",
-                                  dependencies: []),
-                          .testTarget(name: "IntegralSwiftTests",
-                                      dependencies: ["IntegralSwift"])
+                        .target(name: "IntegralSwift",
+                                dependencies: [],
+                                resources: [
+                                    .copy("Resources/PrivacyInfo.xcprivacy")
+                                ]),
+                        .testTarget(name: "IntegralSwiftTests",
+                                    dependencies: ["IntegralSwift"])
                       ])
